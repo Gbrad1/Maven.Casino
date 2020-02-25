b418ceb261f641b2ae3907e8f309bdc94ad369e7
vle <obentoka@gmail.com>
2020-02-25 11:59:33 -0500

    Added some fields

--- src/main/java/io/zipcoder/casino/Craps.java
+++ src/main/java/io/zipcoder/casino/Craps.java
@@ -1,6 +1,17 @@
 package io.zipcoder.casino;
 
-public class Craps {
+import io.zipcoder.casino.dice.Dice;
+import io.zipcoder.casino.player.Player;
 
+public class Craps{
+    private Dice dice;
+    private Player user;
+    private int passLine;
+    private int dontPassLine;
+    private int come;
+    private int dontCome;
+
+    public Craps(Player user){
+    }
 
 }
