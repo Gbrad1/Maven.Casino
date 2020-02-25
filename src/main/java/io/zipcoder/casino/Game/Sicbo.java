ee1d9383bc3e63fb1a7e9a45095d84b9fcb520a0
hlin <Zxsaqw12@&$>
2020-02-25 18:15:01 -0500

    Still working on adding player to sicbo

--- src/main/java/io/zipcoder/casino/Game/Sicbo.java
+++ src/main/java/io/zipcoder/casino/Game/Sicbo.java
@@ -1,36 +1,40 @@
 package io.zipcoder.casino.Game;
 
 import io.zipcoder.casino.dice.Dice;
+import io.zipcoder.casino.player.Player;
 import io.zipcoder.casino.utilities.Console;
 
-import java.util.Arrays;
-import java.util.List;
 
 public class Sicbo {
 
     Console console = new Console(System.in, System.out);
-
     Dice dice = new Dice(3);
-    private final List<Integer> smallRoll = Arrays.asList(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18);
-    private final List<Integer> bigRoll = Arrays.asList(11, 12, 13, 14, 15, 16, 17, 18);
-    private int bigBet;
-    private int smallBet;
+    Player player = new Player();
 
 
     public Sicbo() {
     }
 
-
-    public int newRoll() {
-
-        for (int i = smallRoll.indexOf(0); i < smallRoll.size(); i++) {
-            if (dice.tossAndSum() == smallRoll.indexOf(i)) return smallBet;
-        }
-
-        for (int i = bigRoll.indexOf(0); i < bigRoll.size(); i++) {
-            if (dice.tossAndSum() == bigRoll.indexOf(i)) return bigBet;
-        }
-        return 0;
+    public int getBet() {
+        return console.getIntegerInput("Place bet on Big or Small");
     }
+
+    public boolean inFlavorOfBig() {
+        return dice.tossAndSum() > 10;
+    }
+
+    public boolean inFlavorOfSmall() {
+        return dice.tossAndSum() <= 10;
+    }
+
+    public String getWinner() {
+        String bigWin = "Big Win";
+        String smallWin = "Small Win";
+
+        if (inFlavorOfBig()) return bigWin;
+        else if (inFlavorOfSmall()) return smallWin;
+        return "No roll";
+    }
+
 }
 
