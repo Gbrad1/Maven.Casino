0e42c5b2be832cfd43d7b320da804958bc270e24
asconem <asconem@gmail.com>
2020-02-25 18:25:17 -0500

    Updating Blackjack

--- src/main/java/io/zipcoder/casino/game/Blackjack.java
+++ src/main/java/io/zipcoder/casino/game/Blackjack.java
@@ -4,12 +4,18 @@ import io.zipcoder.casino.card.Deck;
 import io.zipcoder.casino.dealer.BlackjackDealer;
 import io.zipcoder.casino.player.BlackjackPlayer;
 import io.zipcoder.casino.player.Player;
+import io.zipcoder.casino.utilities.Console;
 
 public class Blackjack {
- private Player playerOne;
+ private BlackjackPlayer playerOne;
+ private BlackjackDealer dealerOne;
  private Deck cardDeck;
+ private Console newConsole;
 
     public Blackjack(BlackjackPlayer player, BlackjackDealer dealer){
         this.playerOne = player;
+        this.dealerOne = dealer;
+        this.cardDeck = new Deck();
+        this.newConsole = new Console(System.in, System.out);
     }
 }
