b2c3d4586d5beae538c005b0c6abd47e4e4f019e
hlin <Zxsaqw12@&$>
2020-02-25 14:56:39 -0500

    Add one method

--- src/main/java/io/zipcoder/casino/Game/Sicbo.java
+++ src/main/java/io/zipcoder/casino/Game/Sicbo.java
@@ -1,38 +1,36 @@
 package io.zipcoder.casino.Game;
 
 import io.zipcoder.casino.dice.Dice;
+import io.zipcoder.casino.utilities.Console;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Sicbo {
 
+    Console console = new Console(System.in, System.out);
+
     Dice dice = new Dice(3);
-    private final List<Integer> smallRoll = Arrays.asList(3, 4, 5, 6, 7, 8, 9, 10);
+    private final List<Integer> smallRoll = Arrays.asList(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18);
     private final List<Integer> bigRoll = Arrays.asList(11, 12, 13, 14, 15, 16, 17, 18);
+    private int bigBet;
+    private int smallBet;
 
 
     public Sicbo() {
     }
 
-    public void newRoll() {
+
+    public int newRoll() {
 
         for (int i = smallRoll.indexOf(0); i < smallRoll.size(); i++) {
-            if (dice.tossAndSum() == smallRoll.indexOf(i)) {
-                //smallRoll win;
-            } else {
-                //bigRoll win;
-            }
+            if (dice.tossAndSum() == smallRoll.indexOf(i)) return smallBet;
         }
+
+        for (int i = bigRoll.indexOf(0); i < bigRoll.size(); i++) {
+            if (dice.tossAndSum() == bigRoll.indexOf(i)) return bigBet;
+        }
+        return 0;
     }
 }
 
-
-/*
-if sum == 4
-    then payout 60 * bet;
-
-   maybe switch case;
-
-
- */
