cb021086426b05c6fdfe5eba4a09c1fc053c4a0c
hlin <Zxsaqw12@&$>
2020-02-25 14:32:09 -0500

    Made arraylist

--- src/main/java/io/zipcoder/casino/Game/Sicbo.java
+++ src/main/java/io/zipcoder/casino/Game/Sicbo.java
@@ -1,5 +1,38 @@
 package io.zipcoder.casino.Game;
 
+import io.zipcoder.casino.dice.Dice;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Sicbo {
 
+    Dice dice = new Dice(3);
+    private final List<Integer> smallRoll = Arrays.asList(3, 4, 5, 6, 7, 8, 9, 10);
+    private final List<Integer> bigRoll = Arrays.asList(11, 12, 13, 14, 15, 16, 17, 18);
+
+
+    public Sicbo() {
+    }
+
+    public void newRoll() {
+
+        for (int i = smallRoll.indexOf(0); i < smallRoll.size(); i++) {
+            if (dice.tossAndSum() == smallRoll.indexOf(i)) {
+                //smallRoll win;
+            } else {
+                //bigRoll win;
+            }
+        }
+    }
 }
+
+
+/*
+if sum == 4
+    then payout 60 * bet;
+
+   maybe switch case;
+
+
+ */
---
+++ src/test/java/io/zipcoder/casino/SicboTest.java
@@ -0,0 +1,15 @@
+package io.zipcoder.casino;
+
+import io.zipcoder.casino.dice.Dice;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SicboTest {
+
+    @Test
+    public void getBigSum() {
+        Dice dice = new Dice(3);
+    }
+
+}
