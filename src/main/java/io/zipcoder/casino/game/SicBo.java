92870d3bb0b8c1b8b5d8eb2377d407801d3729cc
hlin <Zxsaqw12@&$>
2020-02-26 16:41:59 -0500

    Add switch case

--- src/main/java/io/zipcoder/casino/game/SicBo.java
+++ src/main/java/io/zipcoder/casino/game/SicBo.java
@@ -10,6 +10,8 @@ public class SicBo implements Gambling {
     private SicBoPlayer player;
     private Console console = new Console(System.in, System.out);
     private Integer input;
+    private Integer bet;
+
 
     public SicBo(SicBoPlayer player) {
         this.player = player;
@@ -20,12 +22,20 @@ public class SicBo implements Gambling {
         return console.getIntegerInput("Enter a bet amount");
     }
 
-    public void play(int bid) {
+    public void play() {
         input = console.getIntegerInput("What would you like to bet on? \n1: Big\n2: Small\n3: Even\n4: Odds\n5: Any Triple");
         switch (input)
         {
             case 1:
                 placeBet();
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
                 break;
         }
     }
