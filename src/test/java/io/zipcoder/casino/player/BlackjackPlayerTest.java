a678aeb7851fa81ebe6e2f98a971bf4d6649dbcd
asconem <asconem@gmail.com>
2020-02-26 18:47:36 -0500

    Adding to BlackjackPlayerTest class

--- src/test/java/io/zipcoder/casino/player/BlackjackPlayerTest.java
+++ src/test/java/io/zipcoder/casino/player/BlackjackPlayerTest.java
@@ -1,6 +1,14 @@
 package io.zipcoder.casino.player;
 
+import org.junit.Test;
+
 public class BlackjackPlayerTest {
+    @Test
+    public void getPlayerTest() {
+        Player player = new Player();
+        BlackjackPlayer blackjackPlayer = new BlackjackPlayer(player);
+        blackjackPlayer.getPlayer();
+    }
 }
 
 
