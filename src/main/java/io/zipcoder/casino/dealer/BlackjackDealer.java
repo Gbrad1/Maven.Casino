bc6e18b790d7eafc5766a7ebb4a428b536c03abe
asconem <asconem@gmail.com>
2020-02-26 11:09:52 -0500

    Updating blackjack counting logic.

--- src/main/java/io/zipcoder/casino/dealer/BlackjackDealer.java
+++ src/main/java/io/zipcoder/casino/dealer/BlackjackDealer.java
@@ -30,16 +30,20 @@ public class BlackjackDealer {
         }
         int score = 0;
         for (int i = 0; i < getHand().size(); i++) {
+            if(getHand().get(i).getRank() > 10) {
+                score += 10;
+            } else {
             score += hand.getHand().get(i).getRank();
+            }
+
         }
-        return score;
+            return score;
     }
 
-    public boolean checkBlackjack(){
+    public boolean checkBlackjack() {
         if (getScore() == 21 && hand.getHand().size() == 2){
             return true;
         }
-        return false;
+            return false;
     }
-
 }
--- src/main/java/io/zipcoder/casino/game/Blackjack.java
+++ src/main/java/io/zipcoder/casino/game/Blackjack.java
@@ -1,5 +1,4 @@
 package io.zipcoder.casino.game;
-
 import io.zipcoder.casino.card.Deck;
 import io.zipcoder.casino.dealer.BlackjackDealer;
 import io.zipcoder.casino.player.BlackjackPlayer;
