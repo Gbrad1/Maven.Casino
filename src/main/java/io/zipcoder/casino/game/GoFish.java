ade08b070f8f2fca30f358bf6886c15c1cd9263b
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-02-25 20:00:25 -0500

    saving

--- src/main/java/io/zipcoder/casino/game/GoFish.java
+++ src/main/java/io/zipcoder/casino/game/GoFish.java
@@ -1,12 +1,12 @@
 package io.zipcoder.casino.game;
 
 import io.zipcoder.casino.card.Deck;
+import io.zipcoder.casino.player.GoFishPlayer;
 import io.zipcoder.casino.utilities.Console;
 
 public class GoFish {
-    Deck deck;
-    Integer playerScore;
-    Integer dealerScore;
+    private Deck deck;
+    private GoFishPlayer goFishPlayer;
     Console console = new Console(System.in, System.out);
 
     public GoFish() {
